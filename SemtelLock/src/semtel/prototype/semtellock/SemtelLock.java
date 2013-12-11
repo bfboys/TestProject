f3d36f4bac19a06dea25371e4326b977ce2e77b3
ejy <ejy@220.116.209.201>
2013-12-12 00:31:33 +0900

    apply AES256

--- SemtelLock/src/semtel/prototype/semtellock/SemtelLock.java
+++ SemtelLock/src/semtel/prototype/semtellock/SemtelLock.java
@@ -1,41 +1,67 @@
 package semtel.prototype.semtellock;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import net.imcore.AES256Cipher;
+
 public class SemtelLock {
 	
-	String password;
+	String key;
+	String encodedPassword;
 	Boolean isLock;
 	
 	public SemtelLock(){
 		
-		//파일로부터 불러와야한다.
+		//to do : key and encodedPassword load in file
 		
-		//password = ..to do..
 		isLock = false;
+		key = "abcdefghijklmnopqrstuvwxyz123456";
+			
 		
 	}
 	
-	public void setPassword(String password){
-		this.password = password;
+	public void setPassword(String pramPassword){
+		
+		
+		try {
+			
+			//apply AES256 
+			this.encodedPassword = AES256Cipher.AES_Encode(pramPassword, key);
+			
+			//to do : encodedPassword store in file
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public Boolean isRight(String param){
+	public Boolean isRight(String paramPassword){
 		//TO DO...
 		//AES256 암호화 과정을 거친 param과 비교를 해야한다.
 		Boolean result = false;
+			
 		
-		if(password.equals(param)){
-			result = true;
-		}
+		try {
+			//apply AES256 
+			if(encodedPassword.equals(AES256Cipher.AES_Encode(paramPassword, key))){
+				result = true;
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		} 
 	
 		return result;
 	}
